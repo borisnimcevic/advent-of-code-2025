91bb371bb50fb1f5ea62ed0c8f9d0572090d36ba
Boris Nimcevic <borisnimcevic@gmail.com>
2025-12-02 01:16:28 +0100

    day 1 silver star

--- day01/python/day01.py
+++ day01/python/day01.py
@@ -2,18 +2,17 @@ start_position=50
 current_position=start_position
 counter = 0
 
-with open("example.txt","r") as file:
+with open("../input1.txt","r") as file:
+# with open("../example.txt","r") as file:
     for line in file:
         directon=line[0]
         number=line[1:]
         if directon=='R':
             current_position+=int(number)
-            if(current_position>99):
-                current_position-=100
         else:
             current_position-=int(number)
-            if(current_position<0):
-                current_position+=100
+        
+        current_position%=100
 
         if current_position==0:
             counter+=1

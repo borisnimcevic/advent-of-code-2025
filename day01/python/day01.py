c340e9f671e5ee9cdfa02279fe4e7dde31df7735
Boris Nimcevic <borisnimcevic@gmail.com>
2025-12-02 02:02:31 +0100

    day 1 gold star

--- day01/python/day01.py
+++ day01/python/day01.py
@@ -1,6 +1,7 @@
 start_position=50
 current_position=start_position
-counter = 0
+one_counter = 0
+two_counter = 0
 
 with open("../input1.txt","r") as file:
 # with open("../example.txt","r") as file:
@@ -8,14 +9,25 @@ with open("../input1.txt","r") as file:
         directon=line[0]
         number=line[1:]
         if directon=='R':
-            current_position+=int(number)
+            for x in range(int(number)):
+                current_position+=1
+                if(current_position==0):
+                    two_counter+=1
+                if(current_position>99):
+                    two_counter+=1
+                    current_position-=100
         else:
-            current_position-=int(number)
-        
-        current_position%=100
+            for x in range(int(number)):
+                current_position-=1
+                if(current_position==0):
+                    two_counter+=1
+                if(current_position<0):
+                    # two_counter+=1
+                    current_position+=100
 
-        if current_position==0:
-            counter+=1
+        if(current_position==0):
+           one_counter+=1
+        
 
         # print(directon,end="")
         # print()
@@ -24,4 +36,5 @@ with open("../input1.txt","r") as file:
         # print(current_position,end="")
         # print()
 
-print(counter)
+print(one_counter)
+print(two_counter)
